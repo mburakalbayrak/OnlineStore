f3a7bd26a81252c95bf5415a6891cd54a5bda2b1
Burak ALBAYRAK <burakalbayrak_34@hotmail.com>
2018-01-27 12:30:36 +0300

    NO-ISSUE / Core katmanı tammalandı

---
+++ OnlineStore.Core/ConfigurationHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OnlineStore.Core.Common.Contracts;
+
+namespace OnlineStore.Core
+{
+    public class ConfigurationHelper : IConfigurationHelper
+    {
+    }
+}
