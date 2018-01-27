f91635e20e0a0423cef36b0442e10e88c914db05
Burak ALBAYRAK <burakalbayrak_34@hotmail.com>
2018-01-27 13:04:54 +0300

    Core.Common.Caching ve Core.Common.Logging katmanları tamamlandı.

---
+++ OnlineStore.Core.Common.Caching/CacheProvider.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+using OnlineStore.Core.Common.Contracts;
+
+namespace OnlineStore.Core.Common.Caching
+{
+    public class CacheProvider:ICacheProvider
+    {
+        private readonly MemoryCache _cache = MemoryCache.Default;
+
+        public void Set(string key, object value, int expireAsMinute)
+        {
+            System.Diagnostics.Contracts.Contract.Assert(!string.IsNullOrEmpty(key)); //key değerinin null veya boş olmadığından emin ol, yoksa exception fırlat
+            System.Diagnostics.Contracts.Contract.Assert(value != null);  //value değerinin null olmadığından emin ol, yoksa exception fırlat
+            System.Diagnostics.Contracts.Contract.Assert(expireAsMinute > 0); //expireAsMinute değerinin 0'dan büyük olduğuna emin ol, yoksa exception fırlat
+            if (IsExist(key))
+            {
+                Remove(key);
+            }
+
+            _cache.Add(key, value, DateTimeOffset.Now.AddMinutes(expireAsMinute));
+        }
+
+        public T Get<T>(string key)
+        {
+            System.Diagnostics.Contracts.Contract.Assert(!string.IsNullOrEmpty(key)); //key değerinin null veya boş olmadığından emin ol, yoksa exception fırlat
+            return (T) _cache.Get(key);
+        }
+
+        public void Remove(string key)
+        {
+            System.Diagnostics.Contracts.Contract.Assert(!string.IsNullOrEmpty(key)); //key değerinin null veya boş olmadığından emin ol, yoksa exception fırlat
+            _cache.Remove(key);
+        }
+
+        public bool IsExist(string key)
+        {
+            System.Diagnostics.Contracts.Contract.Assert(!string.IsNullOrEmpty(key)); //key değerinin null veya boş olmadığından emin ol, yoksa exception fırlat
+            return _cache.Any(i => i.Key == key);
+        }
+    }
+}
---
+++ OnlineStore.Core.Common.Logging/Logger.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+using OnlineStore.Core.Common.Contracts;
+
+namespace OnlineStore.Core.Common.Logging
+{
+    public class Logger:ILogger
+    {
+        private readonly log4net.ILog _log;
+
+        public Logger()
+        {
+            log4net.Config.XmlConfigurator.Configure(new FileInfo(AppDomain.CurrentDomain.BaseDirectory + "log4net.config"));
+
+            _log = log4net.LogManager.GetLogger(GetType().FullName);
+        }
+
+        public void Info(string message, Dictionary<string, string> additionalColums = null)
+        {
+            System.Diagnostics.Contracts.Contract.Assert(!string.IsNullOrEmpty(message)); //message değerinin null veya boş olmadığından emin ol, yoksa exception fırlat
+
+            BindAdditionalColumsIfNotEmpty(additionalColums);
+
+            _log.Info(message);
+        }
+
+        public void Error(string message, Exception exception, Dictionary<string, string> additionalColums = null)
+        {
+            System.Diagnostics.Contracts.Contract.Assert(!string.IsNullOrEmpty(message)); //message değerinin null veya boş olmadığından emin ol, yoksa exception fırlat
+            System.Diagnostics.Contracts.Contract.Assert(exception != null); //exception değerinin null veya boş olmadığından emin ol, yoksa exception fırlat
+
+            BindAdditionalColumsIfNotEmpty(additionalColums);
+
+            _log.Error(message, exception);
+        }
+
+        private void BindAdditionalColumsIfNotEmpty(Dictionary<string, string> additionalColums)
+        {
+            if (additionalColums != null && additionalColums.Any())
+            {
+                foreach (var column in additionalColums)
+                {
+                    log4net.ThreadContext.Properties[column.Key] = column.Value;
+                }
+            }
+        }
+    }
+}
