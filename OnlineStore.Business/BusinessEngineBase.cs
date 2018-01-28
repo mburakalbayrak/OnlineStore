c92263033cdc7cdb9633212389b78f202c07344a
Burak ALBAYRAK <burakalbayrak_34@hotmail.com>
2018-01-28 15:09:58 +0300

    NO-ISSUE / BLL katman geliştirmesi

---
+++ OnlineStore.Business/BusinessEngineBase.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OnlineStore.Core.Common.Contracts;
+
+namespace OnlineStore.Business
+{
+    public class BusinessEngineBase
+    {
+        public IConfigurationHelper ConfigurationHelper;
+        private readonly ILogger _logger;
+
+        public BusinessEngineBase()
+        {
+            // TODO: DI Framework eklendikten sonra bu field'lar initialize edilecek.
+        }
+
+        protected T ExecuteWithExceptionHandledOperation<T>(Func<T> func)
+        {
+            try
+            {
+                var result = func.Invoke();
+
+                return result;
+            }
+            catch (Exception exception)
+            {
+                _logger.Error(exception.Message, exception);
+                throw;
+            }
+        }
+
+        protected void ExecuteWithExceptionHandledOperation(Action action)
+        {
+            try
+            {
+                action.Invoke();
+            }
+            catch (Exception exception)
+            {
+                _logger.Error(exception.Message, exception);
+                throw;
+            }
+        }
+    }
+}
--- OnlineStore.Business/ProductEngine.cs
+++ OnlineStore.Business/ProductEngine.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace OnlineStore.Business
 {
-    public class Class1
+    public class ProductEngine : BusinessEngineBase
     {
     }
 }
