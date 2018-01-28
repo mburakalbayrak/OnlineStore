88454436e0890c5e8527430dba10341d8f4ac745
Burak ALBAYRAK <burakalbayrak_34@hotmail.com>
2018-01-28 13:56:36 +0300

    NO-ISSUE * Data.Contracts katmanı eklendi. Data katmanında RepositoryBase sınıfında IRepository interface i implemente edildi.

---
+++ OnlineStore.Data.Contracts/IRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore.Data.Contracts
+{
+    public interface IRepository<TEntity, TKey>
+        where TEntity : class
+    {
+        Task<TEntity> GetAsync(TKey id);
+
+        IQueryable<TEntity> GetAll(int skip, int take);
+
+        IQueryable<TEntity> GetAll(int skip, int take, Expression<Func<TEntity, bool>> predicate);
+
+        void Add(TEntity entity);
+
+        void Update(TEntity entity);
+
+        void Update(TKey id);
+
+        void Delete(TEntity entity);
+
+        Task SaveChangeAsync();
+
+    }
+}
